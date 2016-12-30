d8d6d135e06802aca760f64384c90370322c1cbf
speShow <942865203@qq.com>
2016-12-30 19:32:00 +0800

    commit

---
+++ scripts/brandsData.js
@@ -0,0 +1,347 @@
+var carData=[
+	{
+		title:"A",
+		brands:[
+			{
+				brandName:{name:"阿尔法罗密欧",href:""},
+				brandType:[
+					{name:"ALFA156",href:""},
+					{name:"ALFA166",href:""},
+					{name:"ALFA GT",href:""}
+				]
+			},
+			{
+				brandName:{name:"阿斯顿·马丁",href:""},
+				brandType:[
+					{name:"V8 Vantage",href:""},
+					{name:"Vanquish",href:""},
+					{name:"Rapide",href:""}
+				]
+			},
+			{
+				brandName:{name:"奥迪",href:""},
+				brandType:[
+					{name:"奥迪A6L",href:""},
+					{name:"奥迪A4L",href:""},
+					{name:"奥迪Q7",href:""}
+				]
+			},
+			{
+				brandName:{name:"安凯客车",href:""},
+				brandType:[
+					{name:"宝斯通",href:""}
+				]
+			}
+		]
+	},
+	// {
+	// 	title:"B",
+	// 	brands:[
+	// 		{name:"宝骏",href:""},
+	// 		{name:"宝马",href:""},
+	// 		{name:"保时捷",href:""},
+	// 		{name:"北京汽车",href:""},
+	// 		{name:"北汽制造",href:""},
+	// 		{name:"奔驰",href:""},
+	// 		{name:"奔腾",href:""},
+	// 		{name:"本田",href:""},
+	// 		{name:"比亚迪",href:""},
+	// 		{name:"标致",href:""},
+	// 		{name:"别克",href:""},
+	// 		{name:"宾利",href:""},
+	// 		{name:"布加迪",href:""},
+	// 		{name:"巴博斯",href:""},
+	// 		{name:"宾仕盾",href:""},
+	// 		{name:"宝沃",href:""},
+	// 		{name:"北汽新能源",href:""},
+	// 		{name:"北汽威旺",href:""},
+	// 		{name:"北汽幻速",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"C",
+	// 	brands:[
+	// 		{name:"昌河",href:""},
+	// 		{name:"长安",href:""},
+	// 		{name:"长城",href:""},
+	// 		{name:"成功汽车",href:""},
+	// 		{name:"长安商用",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"D",
+	// 	brands:[
+	// 		{name:"大众",href:""},
+	// 		{name:"道奇",href:""},
+	// 		{name:"东风",href:""},
+	// 		{name:"东南",href:""},
+	// 		{name:"大宇",href:""},
+	// 		{name:"东风风光",href:""},
+	// 		{name:"大迪",href:""},
+	// 		{name:"大发",href:""},
+	// 		{name:"东风风度",href:""},
+	// 		{name:"东风风神",href:""},
+	// 		{name:"东风小康",href:""},
+	// 		{name:"DS",href:""},
+	// 		{name:"东风风行",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"F",
+	// 	brands:[
+	// 		{name:"法拉利",href:""},
+	// 		{name:"菲亚特",href:""},
+	// 		{name:"丰田",href:""},
+	// 		{name:"福迪",href:""},
+	// 		{name:"福特",href:""},
+	// 		{name:"福田",href:""},
+	// 		{name:"富奇",href:""},
+	// 		{name:"菲斯克",href:""},
+	// 		{name:"福汽启腾",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"G",
+	// 	brands:[
+	// 		{name:"GMC",href:""},
+	// 		{name:"广汽传祺",href:""},
+	// 		{name:"广汽吉奥",href:""},
+	// 		{name:"观致",href:""},
+	// 		{name:"光冈",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"H",
+	// 	brands:[
+	// 		{name:"哈飞",href:""},
+	// 		{name:"海马",href:""},
+	// 		{name:"悍马",href:""},
+	// 		{name:"红旗",href:""},
+	// 		{name:"华普",href:""},
+	// 		{name:"华泰",href:""},
+	// 		{name:"黄海",href:""},
+	// 		{name:"华颂",href:""},
+	// 		{name:"海格",href:""},
+	// 		{name:"华凯",href:""},
+	// 		{name:"汉江",href:""},
+	// 		{name:"哈弗",href:""},
+	// 		{name:"恒天",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"J",
+	// 	brands:[
+	// 		{name:"吉利汽车",href:""},
+	// 		{name:"Jeep",href:""},
+	// 		{name:"江淮",href:""},
+	// 		{name:"江铃",href:""},
+	// 		{name:"捷豹",href:""},
+	// 		{name:"金杯",href:""},
+	// 		{name:"金程",href:""},
+	// 		{name:"金龙",href:""},
+	// 		{name:"江铃集团轻汽",href:""},
+	// 		{name:"金旅",href:""},
+	// 		{name:"九龙",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"K",
+	// 	brands:[
+	// 		{name:"开瑞",href:""},
+	// 		{name:"凯迪拉克",href:""},
+	// 		{name:"克莱斯勒",href:""},
+	// 		{name:"康迪",href:""},
+	// 		{name:"卡尔森",href:""},
+	// 		{name:"科尼赛克",href:""},
+	// 		{name:"凯翼",href:""},
+	// 		{name:"KTM",href:""},
+	// 		{name:"卡威",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"L",
+	// 	brands:[
+	// 		{name:"兰博基尼",href:""},
+	// 		{name:"劳斯莱斯",href:""},
+	// 		{name:"雷克萨斯",href:""},
+	// 		{name:"雷诺",href:""},
+	// 		{name:"力帆",href:""},
+	// 		{name:"莲花汽车",href:""},
+	// 		{name:"林肯",href:""},
+	// 		{name:"铃木",href:""},
+	// 		{name:"陆风",href:""},
+	// 		{name:"路虎",href:""},
+	// 		{name:"猎豹汽车",href:""},
+	// 		{name:"罗孚",href:""},
+	// 		{name:"老爷车",href:""},
+	// 		{name:"理念",href:""},
+	// 		{name:"路特斯",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"M",
+	// 	brands:[
+	// 		{name:"马自达",href:""},
+	// 		{name:"玛莎拉蒂",href:""},
+	// 		{name:"雷迈巴赫",href:""},
+	// 		{name:"MINI",href:""},
+	// 		{name:"MG",href:""},
+	// 		{name:"美亚",href:""},
+	// 		{name:"迈凯伦",href:""},
+	// 		{name:"摩根",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"N",
+	// 	brands:[
+	// 		{name:"纳智捷",href:""},
+	// 		{name:"尼奥普兰",href:""},
+	// 		{name:"南京金龙",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"O",
+	// 	brands:[
+	// 		{name:"讴歌",href:""},
+	// 		{name:"欧宝",href:""},
+	// 		{name:"欧朗",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"P",
+	// 	brands:[
+	// 		{name:"帕卡德",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"Q",
+	// 	brands:[
+	// 		{name:"奇瑞",href:""},
+	// 		{name:"起亚",href:""},
+	// 		{name:"乔治·巴顿",href:""},
+	// 		{name:"启辰",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"R",
+	// 	brands:[
+	// 		{name:"日产",href:""},
+	// 		{name:"荣威",href:""},
+	// 		{name:"瑞麒",href:""},
+	// 		{name:"如虎",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"S",
+	// 	brands:[
+	// 		{name:"萨博",href:""},
+	// 		{name:"三菱",href:""},
+	// 		{name:"双环",href:""},
+	// 		{name:"双龙",href:""},
+	// 		{name:"斯巴鲁",href:""},
+	// 		{name:"斯柯达",href:""},
+	// 		{name:"smart",href:""},
+	// 		{name:"上汽大通",href:""},
+	// 		{name:"陕汽通家",href:""},
+	// 		{name:"赛麟",href:""},
+	// 		{name:"思派朗",href:""},
+	// 		{name:"绅宝",href:""},
+	// 		{name:"世爵",href:""},
+	// 		{name:"思铭",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"T",
+	// 	brands:[
+	// 		{name:"泰卡特",href:""},
+	// 		{name:"特斯拉",href:""},
+	// 		{name:"腾势",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"W",
+	// 	brands:[
+	// 		{name:"威麟",href:""},
+	// 		{name:"威兹曼",href:""},
+	// 		{name:"沃尔沃",href:""},
+	// 		{name:"五菱汽车",href:""},
+	// 		{name:"五十铃",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"X",
+	// 	brands:[
+	// 		{name:"西雅特",href:""},
+	// 		{name:"现代",href:""},
+	// 		{name:"雪佛兰",href:""},
+	// 		{name:"雪铁龙",href:""},
+	// 		{name:"新凯",href:""}
+	// 	]
+	// },
+	// {
+	// 	title:"Y",
+	// 	brands:[
+	// 		{name:"一汽",href:""},
+	// 		{name:"依维柯",href:""},
+	// 		{name:"英菲尼迪",href:""},
+	// 		{name:"永源",href:""},
+	// 		{name:"野马汽车",href:""},
+	// 		{name:"云雀",href:""},
+	// 		{name:"英致",href:""}
+	// 	]
+	// },
+	{
+		title:"Z",
+		brands:[
+			{
+				brandName:{name:"中华",href:""},
+				brandType:[
+					{name:"中华骏捷",href:""},
+					{name:"中华酷宝",href:""},
+					{name:"中华尊驰",href:""}
+				]
+			},
+			{
+				brandName:{name:"中兴",href:""},
+				brandType:[
+					{name:"旗舰A9",href:""},
+					{name:"威虎G3",href:""},
+					{name:"昌铃",href:""}
+				]
+			},
+			{
+				brandName:{name:"众泰",href:""},
+				brandType:[
+					{name:"众泰Z100",href:""},
+					{name:"众泰5008",href:""},
+					{name:"众泰Z700",href:""}
+				]
+			},
+			{
+				brandName:{name:"中顺",href:""},
+				brandType:[
+					{name:"中顺世纪",href:""}
+				]
+			},
+			{
+				brandName:{name:"中欧汽车",href:""},
+				brandType:[
+					{name:"中欧奔驰汽车",href:""}
+				]
+			},
+			{
+				brandName:{name:"知豆",href:""},
+				brandType:[
+					{name:"知豆D1",href:""},
+					{name:"知豆D2",href:""},
+					{name:"知豆",href:""}
+				]
+			}
+		]
+	}
+];
+function createCars(item){
+	var div=getDiv("car-item clear");
+	div.appendChild(getSpan(item.title,"car-letter"));
+	return div;
+}
